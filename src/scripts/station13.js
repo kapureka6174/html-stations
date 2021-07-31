91a3bb3a9cc0cf13963d6f3c7e0455dfd337aac0
kapureka6174 <mitsu.n.0109@gmail.com>
2021-07-31 11:49:21 +0900

    Update station13.js

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,7 @@
 function getData() {
-    // Arrowという文字列を返す処理をアロー関数を入れて書きましょう。
-    return // ここにアロー関数の処理を書く
+  // Arrowという文字列を返す処理をアロー関数を入れて書きましょう。
+  const arrow = () => {
+    return "Arrow";
+  };
+  return arrow(); // ここにアロー関数の処理を書く
 }
